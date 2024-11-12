d15ce9a3a3a6e901855e543df75861265239c1bf
prabhat <prabhatshukla0849@gmail.com>
2024-11-12 23:47:59 +0530

    ws logic server implemented

---
+++ apps/server/src/index.ts
@@ -0,0 +1,75 @@
+import {WebSocket} from "ws"
+import http from "http"
+import cors from "cors"
+
+var httpServer = http.createServer(function (request: any, response: any) {
+  console.log((new Date()) + ' Received request for ' + request.url);
+  response.writeHead(404);
+  response.end();
+});
+
+
+const wss = new WebSocket.Server({ server: httpServer });
+
+interface User {
+  ws: WebSocket;
+  online: boolean;
+  playerName?: string;
+  playing?: boolean;
+}
+
+const allUsers: { [key: string]: User } = {};
+
+const allRooms = []
+
+function generateUniqueId() {
+  return Math.random().toString(36).substr(2, 9);
+}
+
+
+wss.on("connection" , (ws) =>{
+
+  const userId = generateUniqueId();
+   
+  allUsers[userId] = {
+    ws,
+    online: true,
+  };
+
+
+  ws.on('request to play' ,(data)=>{
+    const currentUser = allUsers[userId]
+    currentUser!.playerName = data.playerName
+
+    let opponentPlayer
+
+    for (const key in allUsers) {
+      const user = allUsers[key];
+      if(user){
+        if (user.online && !user.playing && user.ws !== ws) {
+          opponentPlayer = user;
+          break;
+        }
+      }
+    }
+
+    if(opponentPlayer){
+      allRooms.push({
+        player1: opponentPlayer,
+        player2: currentUser
+      })
+
+      currentUser?.ws.send("OpponentFound" , {
+        opponentName : opponentPlayer.playerName,
+        playingAs : "circle"
+      })
+    }
+
+  } )
+}) 
+
+
+httpServer.listen(8000, function () {
+  console.log((new Date()) + ' Server running at http://127.0.0.8000')
+}
+)
