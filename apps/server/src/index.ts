037e15d731634671a2e62a7d4b644c2876758fbd
prabhat <prabhatshukla0849@gmail.com>
2024-11-27 21:14:28 +0530

    some changes

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -3,7 +3,7 @@ import http from "http"
 
 
 
-const wss = new WebSocketServer({port: 8000})
+const wss = new WebSocketServer({ port: 8000 })
 
 
 
@@ -19,7 +19,7 @@ interface User {
 
 const allUsers: { [key: string]: User } = {};
 
-const allRooms: Array<{ player1: User; player2: User}> = [];
+const allRooms: Array<{ player1: User; player2: User }> = [];
 
 function generateUniqueId() {
   return Math.random().toString(36).substr(2, 9);
@@ -39,14 +39,14 @@ wss.on("connection", (ws) => {
 
   ws.send(JSON.stringify({ type: 'welcome', message: 'Welcome to the WebSocket server!' }));
 
-  Object.keys(allUsers).forEach((userId)=>{
-    if(allUsers[userId]?.ws!=ws){
+  Object.keys(allUsers).forEach((userId) => {
+    if (allUsers[userId]?.ws != ws) {
       allUsers[userId]!.ws.send("new User Joined")
     }
   })
 
- 
- 
+
+
 
   // ws.on("bros", (data)=>{
   //    ws.send("hello")
@@ -58,20 +58,64 @@ wss.on("connection", (ws) => {
 
     const data = message.toString()
     // const data = JSON.parse(message.toString());
-    const  obj = JSON.parse(data)
+    const obj = JSON.parse(data)
 
     if (obj.type === "bros") {
-        console.log("Bros event triggered with data:", obj.payload);
-        ws.send("hello");
+      console.log("Bros event triggered with data:", obj.payload);
+      ws.send("fuck it");
 
-        Object.keys(allUsers).forEach((userId)=>{
-          if(allUsers[userId]?.ws!=ws){
-             let gh = JSON.stringify(obj.payload)
-            allUsers[userId]!.ws.send(`message received ${gh}`)
-          }
-        })
+      Object.keys(allUsers).forEach((userId) => {
+        if (allUsers[userId]?.ws != ws) {
+          let gh = JSON.stringify(obj.payload)
+          allUsers[userId]!.ws.send(`message received ${gh}`)
+        }
+      })
     }
-});
+
+    if (obj.type === "request to play") {
+
+      const currentUser = allUsers[userId]
+      currentUser!.playerName = obj.playerName
+
+      let opponentPlayer
+
+      for (const userId in allUsers) {
+        if (allUsers[userId]?.ws !== ws && allUsers[userId]?.online && !allUsers[userId].playing) {
+          opponentPlayer = allUsers[userId];
+          opponentPlayer.playing = true
+          currentUser!.playing=true
+          break;
+
+        }
+
+      }
+
+      if (opponentPlayer && currentUser) {
+        allRooms.push({
+          player1: currentUser,
+          player2: opponentPlayer,
+        })
+
+        currentUser?.ws.send(
+          JSON.stringify({
+            type: "OpponentFound",
+            opponentName: opponentPlayer.playerName,
+            playingAs: "circle",
+          })
+        );
+
+        opponentPlayer?.ws.send(
+          JSON.stringify({
+            type: "OpponentFound",
+            opponentName: currentUser!.playerName,
+            playingAs: "cross",
+          })
+        );
+      }
+
+
+    }
+  });
 
   // ws.on('request to play', (data) => {
   //   const currentUser = allUsers[userId]
@@ -91,7 +135,7 @@ wss.on("connection", (ws) => {
 
   //   if (opponentPlayer && currentUser) {
   //     allRooms.push({
-  //       player1: opponentPlayer ,
+  //       player1: opponentPlayer,
   //       player2: currentUser
   //     })
 
@@ -131,38 +175,38 @@ wss.on("connection", (ws) => {
   //   }
 
   // })
-  
-  ws.on("close", function(){
+
+  ws.on("close", function () {
     const currentUser = allUsers[userId]
-    currentUser!.online= false
+    currentUser!.online = false
     currentUser!.playing = false
 
 
-  
-
-    for(let index=0; index<allRooms.length; index++ ){
-
-      const room =  allRooms[index]
-      
-      if(room && room.player1 && room.player2){
-        const {player1, player2} = room
 
 
-      if (player1.ws === ws) {
-        player2.ws.send(JSON.stringify({ type: "opponentLeftMatch" }));
-        allRooms.splice(index, 1);
-        break;
-      } else if (player2.ws === ws) {
-        player1.ws.send(JSON.stringify({ type: "opponentLeftMatch" }));
-        allRooms.splice(index, 1);
-      
+    for (let index = 0; index < allRooms.length; index++) {
 
-        break;
-      }
+      const room = allRooms[index]
+
+      if (room && room.player1 && room.player2) {
+        const { player1, player2 } = room
+
+
+        if (player1.ws === ws) {
+          player2.ws.send(JSON.stringify({ type: "opponentLeftMatch" }));
+          allRooms.splice(index, 1);
+          break;
+        } else if (player2.ws === ws) {
+          player1.ws.send(JSON.stringify({ type: "opponentLeftMatch" }));
+          allRooms.splice(index, 1);
+
+
+          break;
+        }
       }
     }
   }
- )
+  )
 
 });
 
