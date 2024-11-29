0fa9ca7dae080cf5394965bc882f2175986221cd
prabhat <prabhatshukla0849@gmail.com>
2024-11-29 14:42:49 +0530

    feat:checkWinner function implemented

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -9,13 +9,65 @@ const renderFrom = [
 
 
 const App = () =>{
-   const [ gamestate, setGamestate] = useState(renderFrom);
+   const [ gameState, setGamestate] = useState(renderFrom);
 
    const [currentPlayer , setCurrentPlayer] = useState("circle");
 
    const [finishedState, setFinishetState] = useState(false);
 
-   const [finishedArrayState, setFinishedArrayState] = useState([]);
+   const [finishedArrayState, setFinishedArrayState] = useState<number[]>([]);
+
+   const [playOnline, setPlayOnline] = useState(false);
+
+
+   const checkWinner =()=>{
+    
+    for (let row = 0; row < gameState.length; row++) {
+      if (
+        gameState[row][0] === gameState[row][1] &&
+        gameState[row][1] === gameState[row][2]
+      ) {
+        setFinishedArrayState([row * 3 + 0, row * 3 + 1, row * 3 + 2]);
+        return gameState[row][0];
+      }
+    }
+
+    for (let col = 0; col < gameState.length; col++) {
+      if (
+        gameState[0][col] === gameState[1][col] &&
+        gameState[1][col] === gameState[2][col]
+      ) {
+        setFinishedArrayState([0 * 3 + col, 1 * 3 + col, 2 * 3 + col]);
+        return gameState[0][col];
+      }
+    }
+
+    if (
+      gameState[0][0] === gameState[1][1] &&
+      gameState[1][1] === gameState[2][2]
+    ) {
+      return gameState[0][0];
+    }
+
+    if (
+      gameState[0][2] === gameState[1][1] &&
+      gameState[1][1] === gameState[2][0]
+    ) {
+      return gameState[0][2];
+    }
+   
+
+    const isDrawMatch = gameState.flat().every((e) => {
+      if (e.toString() === "circle" || e.toString() === "cross") return true;
+    });
+
+    if (isDrawMatch) return "draw";
+
+    return null;
+    
+
+
+   }
 
 
 
